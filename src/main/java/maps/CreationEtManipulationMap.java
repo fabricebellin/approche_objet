c8e8b20b81ed7acd158216143020314a9a372cfb
PamelaCalcei <pecalcei@gmail.com>
2023-04-18 11:41:30 +0200

    TP12 - Exercice 2 - terminé

--- src/main/java/maps/CreationEtManipulationMap.java
+++ src/main/java/maps/CreationEtManipulationMap.java
@@ -10,6 +10,24 @@ public class CreationEtManipulationMap {
 		mapVilles.put(44, "Nantes");
 		mapVilles.put(75, "Paris");
 		mapVilles.put(31, "Toulouse");
+		mapVilles.put(59, "Lille");
+		mapVilles.put(69, "Lyon");
+		mapVilles.put(33, "Bordeaux");
+		
+		//Boucle pour afficher les clés
+		
+		for (Integer cle : mapVilles.keySet()) {
+			System.out.println(cle);
+		}
+		
+		//Boucle pour afficher les valeurs
+		for(String valeur : mapVilles.values()) {
+			System.out.println(valeur);
+		}
+		
+		//Aficher la taille de la map
+		System.out.println(mapVilles.size());
+		
 
 	}
 
