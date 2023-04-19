53b517509d310accd18d87fc5ca42927596dfec9
Alegria <Alegria@DESKTOP-8TA1D5F.lan>
2023-04-19 17:04:35 +0200

    TP 1 equals

--- src/tri/Ville.java
+++ src/tri/Ville.java
@@ -35,8 +35,34 @@ public class Ville{
 		this.nbHabitant = nbHabitant;
 	}
 
-
-
+    @Override
+    public boolean equals(Object obj) {
+    	 if (this == obj) { // Vérifier si l'objet est le même
+             return true;
+         }
+         if (obj == null) { // Vérifier si l'objet est null
+             return false;
+         }
+         if (getClass() != obj.getClass()) { // Vérifier si les classes sont différentes
+             return false;
+         }
+         Ville other = (Ville) obj; // Cast l'objet en Ville
+         if (nbHabitant == null) { // Vérifier si les nombres d'habitants sont nulls
+             if (other.nbHabitant != null) {
+                 return false;
+             }
+         } else if (!nbHabitant.equals(other.nbHabitant)) { // Vérifier si les nombres d'habitants sont différents
+             return false;
+         }
+         if (nom == null) { // Vérifier si les noms sont nulls
+             if (other.nom != null) {
+                 return false;
+             }
+         } else if (!nom.equals(other.nom)) { // Vérifier si les noms sont différents
+             return false;
+         }
+         return true;
+    }
 
 
 
@@ -44,6 +70,8 @@ public class Ville{
 	public String toString() {
 		return " nom=" + nom + ", nbHabitant=" + nbHabitant;
 	}
+	
+	
 	
 
 
