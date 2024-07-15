a13cc703cda1f28c78088885a27fa02d174e4424
fabrice bellin <fabrice.bellin.formasup82@gmail.com>
2024-07-15 15:47:37 +0200

    TestMethodStatic

---
+++ src/main/java/fr/TestMethodStatic.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class TestMethodeStatic {
+
+    public static void main(String[] args) {
+        String chaine = "12";
+        int chaineConvertie = Integer.parseInt(chaine);
+
+        int minRandom = 1;
+        int maxRandom = 100;
+
+        int a = (int) (Math.random()*(maxRandom-minRandom+1)+minRandom);
+        int b = (int) (Math.random()*(maxRandom-minRandom+1)+minRandom);
+
+        System.out.println("Maximum entre " + a + " et " + b + " : " + Integer.max(a, b));
+    }
+
+}
