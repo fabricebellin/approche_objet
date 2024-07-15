80d472c88c3aba45a2ca3a645c32dcf84532e24f
fabrice bellin <fabrice.bellin.formasup82@gmail.com>
2024-07-15 13:57:04 +0200

    Correctif suite tp6

--- src/main/java/fr/entities/TestAdressePostale.java
+++ src/main/java/fr/entities/TestAdressePostale.java
@@ -2,8 +2,8 @@ package entites;
 
 public class TestAdressePostale {
     public static void main(String[] args) {
-        AdressePostale adresse1 = new AdressePostale(120, "Rue de Tulipes", 14500, "Vire");
-        AdressePostale adresse2 = new AdressePostale(5, "des Maréchaux", 44100, "Nantes");
+        AdressePostale adresse1 = new AdressePostale(175, "chemin de bois mesnil", 82290, "Montbeton");
+        AdressePostale adresse2 = new AdressePostale(180, "chemin des bois mesnil", 82290, "Montbeton");
 
         System.out.println("Adresse 1: " + adresse1.numeroRue + " " + adresse1.libelleRue + ", " + adresse1.codePostal + " " + adresse1.ville);
         System.out.println("Adresse 2: " + adresse2.numeroRue + " " + adresse2.libelleRue + ", " + adresse2.codePostal + " " + adresse2.ville);
