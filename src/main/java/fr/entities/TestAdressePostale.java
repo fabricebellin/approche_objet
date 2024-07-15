4c24205fd8e625d074ce31c570135d256c504cc0
fabrice bellin <122107036+fabricebellin@users.noreply.github.com>
2024-07-15 14:55:12 +0200

    Update TestAdressePostale.java

--- src/main/java/fr/entities/TestAdressePostale.java
+++ src/main/java/fr/entities/TestAdressePostale.java
@@ -5,7 +5,7 @@ public class TestAdressePostale {
         //première adresse
         AdressePostale adressePostale1 = new AdressePostale(175,"chemin de bois mesnil", "82290", "Montbeton");
         //première adresse
-        AdressePostale adressePostale2 = new AdressePostale(120,"chemin de bois mesnil", "82290", "Montbeton");
+        AdressePostale adressePostale2 = new AdressePostale(175,"chemin de bois mesnil", "82290", "Montbeton");
 
         affichageAdresse(adressePostale1);
         affichageAdresse(adressePostale2);
