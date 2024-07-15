ec7339d0c80b5f5c21c7eb653f9e6c8430b919b0
fabrice bellin <122107036+fabricebellin@users.noreply.github.com>
2024-07-15 14:46:59 +0200

    Update Personne.java

--- src/main/java/fr/entities2/Personne.java
+++ src/main/java/fr/entities2/Personne.java
@@ -3,13 +3,36 @@ package entites2;
 import entites.AdressePostale;
 
 public class Personne {
+
     public String nom;
     public String prenom;
-    public AdressePostale adresse;
+    public AdressePostale adressePostale;
 
-    public Personne(String nom, String prenom, AdressePostale adresse) {
+    //constructeur
+    public Personne(String nom, String prenom, AdressePostale adressePostale) {
         this.nom = nom;
         this.prenom = prenom;
-        this.adresse = adresse;
+        this.adressePostale = adressePostale;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public AdressePostale getAdressePostale() {
+        return adressePostale;
+    }
+    public void setAdressePostale(AdressePostale adressePostale) {
+        this.adressePostale = adressePostale;
     }
 }
