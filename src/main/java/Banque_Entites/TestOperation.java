636d79765c00f5b96d586dbd1c02facd1092fac9
fabrice bellin <fabrice.bellin.formasup82@gmail.com>
2024-07-16 12:50:01 +0200

    Correction package

--- src/main/java/Banque_Entites/TestOperation.java
+++ src/main/java/Banque_Entites/TestOperation.java
@@ -1,35 +1,30 @@
-package Banque_Entites;
+package Banque_Entites; //
 
-public class TestOperation {
-
+public class TestOperation { // Correction de la typo dans le nom de la classe
     public static void main(String[] args) {
-
         Operation[] listeOperations = new Operation[6];
+
         listeOperations[0] = new Credit("11/04/2023", 500);
-        listeOperations[1] = new Credit("13/04/2023", 1000);
+        listeOperations[1] = new Credit("15/04/2023", 1000);
         listeOperations[2] = new Debit("14/04/2023", 200);
         listeOperations[3] = new Credit("11/04/2023", 50);
         listeOperations[4] = new Debit("13/04/2023", 500);
         listeOperations[5] = new Debit("14/04/2023", 130);
+
         int montantGlobal = 0;
 
-        for(Operation uneOperation : listeOperations) {
+        for (Operation uneOperation : listeOperations) {
             System.out.println(uneOperation.toString());
-            if(uneOperation.getType() == "DEBIT") {
+
+            if (uneOperation.getType().equals("DEBIT")) {
                 montantGlobal -= uneOperation.getMontantOperation();
             } else {
                 montantGlobal += uneOperation.getMontantOperation();
             }
         }
 
-        if(montantGlobal > 0) {
-            System.out.println("Montant global : +" + montantGlobal + " €");
-        } else {
-            System.out.println("Montant global : " + montantGlobal + " €");
-        }
-
+        System.out.println("Le solde du compte est de : " + montantGlobal + " euros");
     }
-
 }
 
 
