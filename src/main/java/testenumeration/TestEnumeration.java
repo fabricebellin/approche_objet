64a68a17944fe7817ef5a2b38d0dd8f7829860cd
fabrice bellin <fabrice.bellin.formasup82@gmail.com>
2024-07-17 14:14:59 +0200

    TP 17 JUILLET

--- src/main/java/testenumeration/TestEnumeration.java
+++ src/main/java/testenumeration/TestEnumeration.java
@@ -1,7 +1,5 @@
 package testenumeration;
 
-package fr.diginamic.testenumeration;
-
 public class TestEnumeration {
 
     public static void main(String[] args) {
---
+++ src/test/java/testenumeration/ContinentTest.java
@@ -0,0 +1,6 @@
+package testenumeration;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ContinentTest {
+  
+}
